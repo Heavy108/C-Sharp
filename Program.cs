99d6e9e1e50c2ba5f6b0899ecd71cc0f8ffed22d
Heavy108 <Heavy1089@gmail.com>
2024-10-03 23:17:40 +0530

    first Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte userAge=20;
+            int numberOfEmployees =510;
+            Console.WriteLine(userAge*numberOfEmployees);
+            Console.WriteLine(userAge/numberOfEmployees);
+            Console.WriteLine("Hello, " + "how are you?" + " I love JavaScript" );
+
+            string userInput = Console.ReadLine();
+            Console.WriteLine(userInput.GetType());
+            Console.WriteLine((Convert.ToInt32(userInput)).GetType());
+            // Console.WriteLine(userInput.GetType());
+
+        }
+    }
+
+}
---
+++ second/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace second
+{
+    class secondProgram
+    {
+        static void Main(string[] args)
+         {
+            string userName=" ";
+            int userAge = 0;
+            int currentYear = 0;
+
+            Console.Write("Please enter you fucking name: ");
+            userName =Console.ReadLine();
+            Console.Write("Please enter you  age: ");
+            userAge = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter the current year: ");
+            currentYear = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello World! My name is {0} and I am {1} years old. I was born in {2}.", userName, userAge, currentYear- userAge);
+        }
+    }
+}
